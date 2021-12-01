82067337643d66dfc8f2d288a006cca9f7f56fd1
DESKTOP-Q3OJIKP\daffa <daffaakbar5@gmail.com>
2021-12-01 10:03:20 +0700

    belajar

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Layout from "../components/Layout";
 import Image from "next/image";
-import styles from "../styles/Home.module.css";
+// import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
